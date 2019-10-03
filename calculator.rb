815251ce10e17fb5b7157d0db9bbd14be7c8b323
Karentina Miles <karentina2@hotmail.com>
2019-10-03 12:14:08 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,20 @@
-# Add your variables here
+first_number = get_variable_from_file
+  
+  expect(first_number).to be_an(Integer).or be_a(Float)
+end
+
+second_number = get_variable_from_file
+
+expect(second_number).to be_an(Integer).or be_a(Float)
+expect(second_number).not_to equal(0)
+end
+
+sum = first_number + second_number
+
+expect(sum).to eq(first_number + second_number)
+end
+
+difference = 
+
+expect(difference).to eq(first_number - second_number)
+end
